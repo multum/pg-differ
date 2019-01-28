d8d98c7b51633f5fc20612a49871cd6f28679738
Andrey Vereshchak <andrii.vereshchak@softline.kiev.ua>
2019-01-28 17:36:38 +0200

    add tests

---
+++ test/pg.config.js
@@ -0,0 +1,7 @@
+exports = {
+  host: '172.0.0.1',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'postgres',
+}
---
+++ test/sync/index.test.js
@@ -0,0 +1,18 @@
+const Differ = require('../..')
+const path = require('path')
+const dbConfig = require('../pg.config')
+
+const config = {
+  dbConfig,
+  logging: true,
+  schemaFolder: path.resolve(__dirname, 'schemas'),
+}
+
+describe('sync', () =>
+  it('must be synchronized schemas from the folder "schemas"', async function () {
+    this.timeout(5000)
+
+    const differ = new Differ(config)
+    await differ.sync()
+  }),
+)
