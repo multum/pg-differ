994555f8961aa03e20504369c48b8e3a7d41612d
av-dev <vereschak@gmail.com>
2019-02-16 22:29:27 +0200

    added license to all files

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-present Andrey Vereshchak
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const R = require('ramda')
 
 exports.isExist = R.compose(R.not, R.isNil)
