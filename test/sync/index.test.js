3ffcf14d183508962306eb47816b93816890fdef
av-dev <vereschak@gmail.com>
2019-03-20 13:07:19 +0200

    revert test logging value

--- test/sync/index.test.js
+++ test/sync/index.test.js
@@ -1,7 +1,7 @@
 const Differ = require('../..')
 const path = require('path')
 const dbConfig = require('../pg.config')
-const logging = true
+const logging = Boolean(process.env.TEST_LOGGING)
 
 describe('sync', () => {
   it('sync schemas and seeds', async function () {
